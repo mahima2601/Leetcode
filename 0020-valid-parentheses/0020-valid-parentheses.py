8bba1b4d3215a2a5ec493de6bc0828aeee0f9b57
Mahima kesharwani <88523118+mahima2601@users.noreply.github.com>
2025-02-19 01:50:22 +0530

    Time: 2 ms (60.31%), Space: 12.4 MB (84.46%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def isValid(self, s):
+        stack=[]
+        mirror={")":"(", "}":"{","]":"["}
+        for i in s:
+            if i in {"(","{","["}:
+                stack.append(i)
+            else:
+                if (len(stack)!=0) and mirror[i]==stack[-1]:
+                    stack.pop()
+                else:
+                    return False
+        if len(stack)==0:
+            return True
+        else:
+            return False
+
+
+        
