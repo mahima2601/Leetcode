52daefa3090faea2e0b25d4a511010e75c02bc0e
Mahima kesharwani <88523118+mahima2601@users.noreply.github.com>
2025-02-26 23:29:57 +0530

    Time: 14 ms (68.45%), Space: 13.6 MB (45.83%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,8 +1,6 @@
 class Solution(object):
     def isPalindrome(self, s):
         s_clean= "".join(char for char in s if char.isalnum())
-        print(s_clean[::-1])
-        print(s_clean)
         if (s_clean.lower())==(s_clean[::-1].lower()):
             return True
         else:
