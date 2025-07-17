41a4ce9f37a88ac97b793cc704bca6f388aebbe1
Mahima kesharwani <88523118+mahima2601@users.noreply.github.com>
2025-07-17 16:18:42 +0530

    Time: 10 ms (40.24%), Space: 23.7 MB (5.33%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,8 +1,14 @@
-class Solution(object):
-    def isPalindrome(self, s):
-        s_clean= "".join(char for char in s if char.isalnum())
-        if (s_clean.lower())==(s_clean[::-1].lower()):
-            return True
-        else:
-            return False
-        
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        from collections import deque
+        new_str=[]
+        for i in s:
+            if i.isalnum():
+                new_str.append(i.lower())
+        cleaned=''.join(new_str)
+        d_str=deque(new_str)
+
+        while len(d_str)>1:
+            if d_str.pop() != d_str.popleft():
+                return False
+        return True
