9a92bcf4fa82bbdeb98399788a30e74f7da5f640
Mahima kesharwani <88523118+mahima2601@users.noreply.github.com>
2025-07-21 01:16:05 +0530

    Time: 4 ms (12.87%), Space: 18.9 MB (78.58%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.py
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.py
@@ -6,26 +6,49 @@
 #         self.right = right
 class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
-        if not root:
-            return 0
+        stack = [[root, 1]]
+        res = 0
 
-        level = 0
-        q = deque([root])  # queue for level-order traversal
+        while stack:
+            node, depth = stack.pop()
+            if node:
+                res = max(res, depth)
+                stack.append([node.left, depth + 1])
+                stack.append([node.right, depth + 1])
 
-        while q:
-            for i in range(len(q)):  # iterate through current level
-                node = q.popleft()
-                if node.left:
-                    q.append(node.left)
-                if node.right:
-                    q.append(node.right)
-            level += 1  # level completed
-
-        return level
+        return res
 
 
 
 
+
+
+
+
+
+
+
+
+
+## Method-1: Using BFS iteration-
+# class Solution:
+#     def maxDepth(self, root: Optional[TreeNode]) -> int:
+#         if not root:
+#             return 0
+
+#         level = 0
+#         q = deque([root])  # queue for level-order traversal
+
+#         while q:
+#             for i in range(len(q)):  # iterate through current level
+#                 node = q.popleft()
+#                 if node.left:
+#                     q.append(node.left)
+#                 if node.right:
+#                     q.append(node.right)
+#             level += 1  # level completed
+
+#         return level
 
 
 ## Method-1: Using DFS Recursion
